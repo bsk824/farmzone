e3fc128b637c24e8d1950be6a0f47eb400e37b84
Baek Seungki <bsk824@gmail.com>
2023-04-11 23:07:38 +0900

    수정

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,13 +1,22 @@
 class DragItem {
   constructor(arg) {
     this.wrap = document.querySelector('.cal_body_date');
+    this.dates = this.wrap.querySelectorAll('.item');
     this.item = this.wrap.querySelectorAll('.reg_items');
-    this.detailBox = document.querySelector('.detail_box');
+    this.detailBox = document.querySelector('#detailBox');
     this.dragState = false;
     this.arg = arg;
     this.init();
   }
   init() {
+    this.dates.forEach(_this=>{
+      _this.addEventListener('click',e=>{
+        if(!e.target.closest('.reg_items')) {
+          const target = e.target.closest('.item');
+          if(!target.classList.contains('disDate')) target.classList.toggle('isSelected');
+        }
+      });
+    })
     let dragTimer = null;
     let delta = {
       target: null,
@@ -90,25 +99,59 @@ class DragItem {
         this.detailShow(e);
       });
     }
-    this.detailBox.addEventListener('mouseleave', ()=>{
-      this.detailHide();
-    })
   }
   detailShow(e) {
     if(this.dragState == false) {
       const target = e.target.closest('.reg_items');
-      const rect = target.getBoundingClientRect();
-      this.detailBox.classList.add('isShow');
-      const posTop = rect.top + 20 - (this.detailBox.clientHeight / 2);
-      const posLeft = rect.left + rect.width + 17;
-      this.detailBox.style.cssText = `top:${posTop}px;left:${posLeft}px;`;
+      layerOpen(this.detailBox, target);
     }
-  }
-  detailHide() {
-    this.detailBox.classList.remove('isShow');
   }
 }
 
+function tabScroll() {
+  const tabWrap = document.querySelector('#tabScroll');
+  const tabBtn = tabWrap.querySelectorAll('button');
+  const tagetBox = document.querySelectorAll('.target-box');
+  let scrollState = false;
+  let boxPos = [];
+  let currentNum = 0;
+  tagetBox.forEach(_this=>{
+    const top = _this.getBoundingClientRect().top;
+    boxPos.push(top + window.scrollY - 110);
+  });
+  window.addEventListener('resize', ()=>{
+    boxPos = [];
+    tagetBox.forEach(_this=>{
+      const top = _this.getBoundingClientRect().top;
+      boxPos.push(top + window.scrollY - 110);
+    });
+  });
+  window.addEventListener('scroll',()=>{
+    if(scrollState == false) {
+      for(let idx = 0 ; idx < boxPos.length; idx++) {
+        if(window.scrollY > boxPos[idx]) {
+          let next = (boxPos[idx + 1]) ? boxPos[idx + 1] : document.body.clientHeight;
+          console.log(next);
+          if(window.scrollY < next) currentNum = idx;
+        }
+      }
+      if(!tabBtn[currentNum].closest('li').classList.contains('on')) {
+        tabBtn.forEach(_this => {_this.closest('li').classList.remove('on')})
+        tabBtn[currentNum].closest('li').classList.add('on');
+      }
+    };
+  })
+  tabBtn.forEach((_this, idx)=>{
+    _this.addEventListener('click',()=>{
+      scrollState = true;
+      window.scrollTo(0, boxPos[idx]);
+      tabBtn.forEach(_this => {_this.closest('li').classList.remove('on')})
+      tabBtn[idx].closest('li').classList.add('on');
+      setTimeout(()=>{scrollState = false;},10);
+      
+    })
+  })
+}
 function scrollMov(num) {
   const btn = event.target;
   const wrap = btn.closest('.tab');
@@ -119,7 +162,29 @@ function scrollMov(num) {
   const targets = document.querySelectorAll('.target-box');
   const tops = [];
   targets.forEach(_this=>{
-    tops.push(_this.getBoundingClientRect().top + window.scrollY);
+    tops.push(_this.getBoundingClientRect().top + window.scrollY - 110);
   })
   window.scrollTo(0, tops[num - 1]);
+}
+
+function toggleTr(_this) {
+  _this.closest('tr').classList.toggle('isShow');
+}
+
+
+let layerStateFunc = null;
+
+function layerOpen(layer, target) {
+  if(typeof layer === 'string') layer = document.querySelector(layer);
+  const rect = target.getBoundingClientRect();
+  layer.classList.add('isShow');
+  const posTop = rect.top + 20 - (layer.clientHeight / 2);
+  const posLeft = rect.left + rect.width + 17;
+  layer.style.top = `${posTop}px`;
+  layer.style.left = `${posLeft}px`;
+}
+
+function layerClose(id) {
+  const layer = document.querySelector(id);
+  layer.classList.remove('isShow');
 }
